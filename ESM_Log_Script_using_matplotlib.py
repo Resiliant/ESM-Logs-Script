d99dd3c0058094a00cbbec877fe3ecf79e5dd963
Resiliant <Resiliant@gmail.com>
2017-11-10 13:56:16 -0600

    Data org update
    housekeeping

--- ESM_Log_Script_using_matplotlib.py
+++ ESM_Log_Script_using_matplotlib.py
@@ -11,6 +11,6 @@ import pandas as pd
 #Import .csv log from source folder, TBD by Consultant
 df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
 #Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents Ruleset and corresponding rules/triggers; change title to reflect which Ruleset is being shown
-ax = df.plot(kind = 'bar', title = '47-4000116', subplots = True, sharex = 'True', figsize = (15,9), legend = True, fontsize = 12)
+ax = df.plot(kind = 'bar', title = '47-4000023', subplots = True, sharex = 'True', figsize = (15,9), legend = True, fontsize = 12)
 #Plot graph
 plt.show()
