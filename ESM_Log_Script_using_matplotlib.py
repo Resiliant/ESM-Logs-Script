32bda8735a36afc12dd8e775702d914968272be4
Resiliant <Resiliant@gmail.com>
2017-11-10 12:59:20 -0600

    Organized source data
    Previously working with a sampling of the data.  Reorganized data collection by correlation ruleset with X-axis of graphs representing Ruleset, then corresponding rules/triggers

--- ESM_Log_Script_using_matplotlib.py
+++ ESM_Log_Script_using_matplotlib.py
@@ -7,10 +7,10 @@ import pandas as pd
 #import numpy as np
 
 #Create headers
-headers = ['Matches', 'Firings', 'Retention', 'Time Spent']
-#Import .csv log from source folder
-df = pd.read_csv('esm-log-test.csv')
-#Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents a rule
-ax = df.plot(kind = 'bar', title = 'Test', subplots = True, sharex = 'True', figsize = (15,9), legend = True, fontsize = 12)
+#headers = ['Matches', 'Firings', 'Retention', 'Time Spent']
+#Import .csv log from source folder, TBD by Consultant
+df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
+#Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents Ruleset and corresponding rules/triggers; change title to reflect which Ruleset is being shown
+ax = df.plot(kind = 'bar', title = '47-4000116', subplots = True, sharex = 'True', figsize = (15,9), legend = True, fontsize = 12)
 #Plot graph
 plt.show()
