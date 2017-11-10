2247255fca45d955af966e53c6d1275ae1ecb9ec
Resiliant <Resiliant@gmail.com>
2017-11-10 14:05:29 -0600

    hk'ing

--- Version 1/Graph Correlation Rules.py
+++ Version 1/Graph Correlation Rules.py
@@ -1,13 +1,10 @@
 #ESM Log Pull/Graph
-#Version 1.0 using matplotlib module
+#Version 1.5 using matplotlib module
 
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
-#import numpy as np
 
-#Create headers
-#headers = ['Matches', 'Firings', 'Retention', 'Time Spent']
 #Import .csv log from source folder, TBD by Consultant
 df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
 #Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents Ruleset and corresponding rules/triggers; change title to reflect which Ruleset is being shown
