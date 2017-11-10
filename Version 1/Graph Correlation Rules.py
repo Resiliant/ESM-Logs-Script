d766a9e8cba93c925bc458741e3cca9db72c5db9
Resiliant <Resiliant@gmail.com>
2017-11-10 14:15:09 -0600

    added windows path

--- Version 1/Graph Correlation Rules.py
+++ Version 1/Graph Correlation Rules.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #Import .csv log from source folder, TBD by Consultant
-df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
+#df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
+df = pd.read_csv('C:/Users/rgarbacz/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
 #Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents Ruleset and corresponding rules/triggers; change title to reflect which Ruleset is being shown
 ax = df.plot(kind = 'bar', title = '47-4000023', subplots = True, sharex = 'True', figsize = (15,9), legend = True, fontsize = 12)
 #Plot graph
