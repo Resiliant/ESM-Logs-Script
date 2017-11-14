3e939c89548f83a808fc16177d0d6b569176d0f7
Resiliant <Resiliant@gmail.com>
2017-11-14 15:38:55 -0600

    ongoing changes for labelling

--- Version 1/Graph Correlation Rules.py
+++ Version 1/Graph Correlation Rules.py
@@ -5,14 +5,20 @@ import matplotlib
 import matplotlib.pyplot as plt
 #import matplotlib.patches as patches
 import pandas as pd
-#import numpy as np
+import numpy as np
 
 #Import .csv log from source folder, TBD by Consultant
 #df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
 df = pd.read_csv('C:/Users/rgarbacz/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000116.csv')
-#Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents Ruleset and corresponding rules/triggers; change title to reflect which Ruleset is being shown
-ax = df.plot(kind = 'bar', title = '47-4000116', subplots = True, sharex = 'True', figsize = (15,9), legend = True, fontsize = 12, xticks = True)
 
+#ind = np.arrange(N)
+#LABELS = ["Ruleset", "Rule 1", "Rule 2", "Trigger 1"]
+
+#Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents Ruleset and corresponding rules/triggers; change title to reflect which Ruleset is being shown
+ax = df.plot(kind = 'bar', title = '47-4000116', subplots = True, sharex = 'True', figsize = (15,9), legend = True, fontsize = 12)
+
+#plt.xticks('Ruleset', 'Rule 1', 'Rule 2', 'Trigger 1')
+#xticks(arange(4), ('Ruleset', 'Rule 1', 'Rule 2', 'Trigger 1'))
 
 #set_xticks(df.index)
 #set_xtickslabels(df.C, rotation=90)
