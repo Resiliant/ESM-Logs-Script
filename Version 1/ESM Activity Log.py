ea8ee99b948233c5aaa9043450db9e6de9c6a5ce
Resiliant <Resiliant@gmail.com>
2017-12-07 09:17:30 -0600

    last changes

--- Version 1/ESM Activity Log.py
+++ Version 1/ESM Activity Log.py
@@ -1,21 +1,23 @@
-#ESM Log Pull/Graph
-#Version 1.5 using matplotlib module
+#ESM Activity Log
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import datetime
 
-#DEVELOPER NOTES:
+#   DEVELOPER NOTES:
 #   Will need to explore method to show exact statistical values for each bar instead of determining value from yticks
 
 #Import .csv log from source folder, TBD by Consultant; top path is MacOS, bottom path is Windows
-df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000116.csv')
+df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Types of Logs/ESM Activity Log Slim.csv')
 #df = pd.read_csv('C:/Users/rgarbacz/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000116.csv')
 
 #Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents Ruleset and corresponding rules/triggers; change title to reflect which Ruleset is being shown
-ax = df.plot(kind = 'bar', title = '47-4000116', subplots = True, sharex = 'True', figsize = (15,9), legend = True, fontsize = 12)
+#df = df.astype(str)
+ax = df.plot(kind = 'barh', title = 'Activity Log', figsize = (15,9), legend = True, fontsize = 12)
 #Apply Labels to each column
 plt.xticks( np.arange(4) , ('Ruleset','Rule 1','Rule 2','Trigger 1'), rotation = 0)
 
 #Plot graph
 plt.show()
+
--- Version 1/Graph Correlation Rules as Pie.py
+++ Version 1/Graph Correlation Rules as Pie.py
@@ -12,29 +12,29 @@ import pandas as pd
 import numpy as np
 
 #Import .csv log from source folder, TBD by Consultant; top path is MacOS, bottom path is Windows
-#df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
-df = pd.read_csv('C:/Users/rgarbacz/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000116.csv')
+df = pd.read_csv('/Users/pcmaster/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000023.csv')
+#df = pd.read_csv('C:/Users/rgarbacz/Documents/GitHub/ESM-Logs-Script/Correlation Rulesets/47-4000116.csv')
 
-#labels = [r'Ruleset', r'Rule 1', r'Rule 2', r'Trigger 1']
+labels = [r'Ruleset', r'Rule 1', r'Rule 2', r'Trigger 1']
 
 #Plot from dataframe as bar graph with subplots for each statistic; X-Axis represents Ruleset and corresponding rules/triggers; change title to reflect which Ruleset is being shown
-#ax = df.plot(kind = 'pie', title = '47-4000116', subplots = True, figsize = (18,9), legend = True, fontsize = 12)
-#plt.legend(labels, loc="best")
-#plt.axis('equal')
+ax = df.plot(kind = 'pie', title = '47-4000116', subplots = True, figsize = (18,9), legend = True, fontsize = 12)
+plt.legend(labels, loc="best")
+plt.axis('equal')
 
-#df = pd.DataFrame(3 * np.random.rand(4, 4), index=['a', 'b', 'c', 'd'], 
-#                  columns=['x', 'y','z','w'])
+df = pd.DataFrame(3 * np.random.rand(4, 4), index=['a', 'b', 'c', 'd'],
+                  columns=['x', 'y','z','w'])
 
-plt.style.use('ggplot')
-colors = plt.rcParams['axes.color_cycle']
-values = df['Ruleset', 'Rule 1', 'Rule 2', 'Trigger 1']
-
-fig, axes = plt.subplots(nrows=2, ncols=2)
-for ax, col in zip(axes.flat, df.columns):
-    ax.pie(values = values, labels=df.index, autopct= '%.02f', colors=colors)
-    ax.set(ylabel='', title=col, aspect='equal')
-
-axes[0, 0].legend(bbox_to_anchor=(0, 0.5))
+#plt.style.use('ggplot')
+#colors = plt.rcParams['axes.color_cycle']
+#values = df['Ruleset', 'Rule 1', 'Rule 2', 'Trigger 1']
+#
+#fig, axes = plt.subplots(nrows=2, ncols=2)
+#for ax, col in zip(axes.flat, df.columns):
+#    ax.pie(labels=df.index, autopct= '%.02f', colors=colors)
+#    ax.set(ylabel='', title=col, aspect='equal')
+#
+#axes[0, 0].legend(bbox_to_anchor=(0, 0.5))
 
 
 
